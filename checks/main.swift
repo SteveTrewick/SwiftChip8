4f1ccb3444b487eff26af4ba77915ef4cd5d677d
Steve Trewick <steve.trewick@icloud.com>
2019-01-15 19:29:26 +0000

    add target

---
+++ checks/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  checks
+//
+//  Created by Steve Trewick on 15/01/2019.
+//  Copyright © 2019 Enigmatic Ape. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
