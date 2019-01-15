06a90c361837eb9862481b726f0512d4b233a32a
Steve Trewick <steve.trewick@icloud.com>
2019-01-15 07:19:07 +0000

    Initial Commit

---
+++ SwiftChip8/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftChip8
+//
+//  Created by Steve Trewick on 15/01/2019.
+//  Copyright © 2019 Enigmatic Ape. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
