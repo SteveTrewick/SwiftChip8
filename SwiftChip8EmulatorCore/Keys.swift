d552b00dc9f4ab47e437d1dd7763c49cb974b047
Steve Trewick <steve.trewick@icloud.com>
2019-01-16 10:18:42 +0000

    add keys

---
+++ SwiftChip8EmulatorCore/Keys.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+
+
+class Keys {
+	
+	var values = Array<Bool>(repeating: false, count:0xf)
+	
+	subscript(_ register: Register) -> Bool {
+		get { return values[Int(register.value)] }
+	}
+	
+	subscript(_ hexkey: UInt8) -> Bool {
+		get {return values[Int(hexkey)]}
+		set { values[Int(hexkey)] = newValue }
+	}
+}
