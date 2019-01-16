ed1955a2be760d7643ab85686fe946238274e7fb
Steve Trewick <steve.trewick@icloud.com>
2019-01-16 13:28:45 +0000

    add gpu

--- SwiftChip8EmulatorCore/EmulationError.swift
+++ SwiftChip8EmulatorCore/EmulationError.swift
@@ -5,6 +5,7 @@ enum EmulationError : Error {
 	case badInstruction
 	case loopHalt
 	case emptyStack
+	case gpubork
 }
 
 
---
+++ SwiftChip8EmulatorCore/SwiftChip8GPU.swift
@@ -0,0 +1,38 @@
+
+import Foundation
+
+
+
+
+class SwiftChip8GPU {
+
+	let colorspace = CGColorSpaceCreateDeviceGray()
+	
+	func generateContext(width: Int, height: Int) -> CGContext? {
+		 return CGContext(
+			data            : nil,
+			width           : width,
+			height          : height,
+			bitsPerComponent: 8,
+			bytesPerRow     : width,
+			space           : colorspace,
+			bitmapInfo      : CGImageAlphaInfo.none.rawValue
+		)
+	}
+	
+	func render(buffer: SpriteBuffer) throws -> CGImage {
+		
+		guard let context     = generateContext(width: buffer.width, height: buffer.height) else { fatalError("GPU bork") }
+		guard let pixelbuffer = context.data?.bindMemory(to: UInt8.self, capacity: buffer.contents.count) else { fatalError("GPU bork") }
+		
+		for i in 0..<buffer.contents.count {
+			pixelbuffer[i] = buffer.contents[i] == 0 ? 0 : 255
+		}
+		
+		guard let image = context.makeImage() else { throw EmulationError.gpubork } // make these throw
+	
+		return image
+	}
+	
+	
+}
