2308de563b23409903fb6b14a8f571d1bdae340f
Steve Trewick <steve.trewick@icloud.com>
2019-01-16 10:28:52 +0000

    add bitcollection extension

--- SwiftChip8EmulatorCore/BitCollection.swift
+++ SwiftChip8EmulatorCore/BitCollection.swift
@@ -14,3 +14,25 @@ struct BitCollection<T:FixedWidthInteger>  {
 		}
 	}
 }
+
+
+extension BitCollection : Sequence {
+
+	struct BitCollectionIteraror : IteratorProtocol {
+
+		private let value : T
+		private var shift = Int(T.bitWidth) - 1
+
+		init(value:T) { self.value = value }
+
+		mutating func next() -> UInt8? {
+			if shift < 0 { return nil } else {
+				defer { shift -= 1 }
+				return UInt8((value >> shift) & 0x1)
+			}
+		}
+	}
+	func makeIterator() -> BitCollectionIteraror {
+		return BitCollectionIteraror(value: value)
+	}
+}
