58085d574ef42e448470d1d1a01e5cd6655a92cf
Steve Trewick <steve.trewick@icloud.com>
2019-01-16 12:53:38 +0000

    add displaylink wrapper

--- SwiftChip8/ViewController.swift
+++ SwiftChip8/ViewController.swift
@@ -1,27 +1,71 @@
-//
-//  ViewController.swift
-//  SwiftChip8
-//
-//  Created by Steve Trewick on 15/01/2019.
-//  Copyright © 2019 Enigmatic Ape. All rights reserved.
-//
+
 
 import Cocoa
 
+
+
+class DisplayLink {
+	
+	var link    :CVDisplayLink?
+	var handler : CVDisplayLinkOutputHandler?
+
+	func createLink(screenID: CGDirectDisplayID)  {
+		CVDisplayLinkCreateWithCGDisplay(screenID, &link)
+	}
+	
+	func start()  {
+		CVDisplayLinkSetOutputHandler(link!, handler!)
+		CVDisplayLinkStart(link!)
+	}
+	
+	func stop() {
+		CVDisplayLinkStop(link!)
+	}
+}
+
+
 class ViewController: NSViewController {
 
+	let displayLink = DisplayLink()
+	
+	override func viewDidAppear() {
+		// ok, that's how we do that, then
+		// NB we can't get the screen/window untill viewDidAppear,
+		// didLoad will not cut it as they're not set yet.
+		
+		let idx = NSDeviceDescriptionKey(rawValue: "NSScreenNumber")
+		if let id = self.view.window?.screen?.deviceDescription[idx] as? CGDirectDisplayID {
+			displayLink.createLink(screenID: id)
+			displayLink.handler = { (link, now, out, flags, context) in
+				let fps = 1 / CVDisplayLinkGetActualOutputVideoRefreshPeriod(link)
+				print (fps)
+				return 0
+			}
+			displayLink.start()
+			DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+				self.displayLink.stop()
+			}
+		}
+	
+	}
+
+	
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		// Do any additional setup after loading the view.
 	}
 
 	override var representedObject: Any? {
 		didSet {
-		// Update the view, if already loaded.
+		
 		}
 	}
 
 
 }
+
+
+
 
