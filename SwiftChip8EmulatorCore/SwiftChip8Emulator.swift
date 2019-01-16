54f4140d59af157d284b8a7943f5d04fbf3003c8
Steve Trewick <steve.trewick@icloud.com>
2019-01-16 10:50:36 +0000

    add emu step

--- SwiftChip8EmulatorCore/SwiftChip8Emulator.swift
+++ SwiftChip8EmulatorCore/SwiftChip8Emulator.swift
@@ -1,2 +1,37 @@
 
 import Foundation
+
+
+public typealias MachineDescription = [UInt8 : (MachineState) throws -> Void]
+
+
+
+public class SwiftChip8Emulator {
+
+	let machinecore  : [UInt8 : (MachineState) throws -> Void]
+	let machinestate : MachineState
+	
+	public init(machinecore: [UInt8 : (MachineState) throws -> Void], machinestate: MachineState) {
+		self.machinecore  = machinecore
+		self.machinestate = machinestate
+	}
+
+	
+	func step() throws {
+		
+		let hi         = machinestate.memory[Int(machinestate.pc.pointer)    ]
+		let lo         = machinestate.memory[Int(machinestate.pc.pointer) + 1]
+		let opcode     = (UInt16(hi) << 8) + UInt16(lo)
+		
+		machinestate.opcode = Opcode(word: opcode)
+		
+		guard let execute = machinecore[machinestate.opcode.code] else {
+			throw EmulationError.badInstruction
+		}
+		
+		try execute(machinestate)
+	}
+	
+	
+
+}
